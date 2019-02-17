1b89d8d4083312abf2620fd5ca1ef6f4a66d096f
sasithorn-jai <sasithorn.jai@ku.th>
2019-02-17 11:24:25 +0700

    threeCommit

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form.component.scss']
 })
 export class FormComponent implements OnInit {
-
-  constructor() { }
+  formGroup: FormGroup;
+  constructor(
+    private formBuild: FormBuilder
+  ) {
+ 
+   }
 
   ngOnInit() {
+    this.formGroup = this.formBuild.group({
+      firstName: this.formBuild.control[''],
+      lastName: ['']
+    })
   }
 
 }
