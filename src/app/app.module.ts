d9a3e885ad4c48969f53d78d74316fb89ce5a922
sasithorn-jai <sasithorn.jai@ku.th>
2019-02-17 11:00:24 +0700

    twoCommit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule} from '@angular/forms'
 import { AppComponent } from './app.component';
 import { from } from 'rxjs';
+import { FormComponent } from './form/form.component';
+import { DetailComponent } from './detail/detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent,
+    DetailComponent
   ],
   imports: [
     BrowserModule,
